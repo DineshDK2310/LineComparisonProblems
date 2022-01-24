159fcc321367b4e56091b4bf3b1242cb9450d479
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-01-23 17:54:08 -0800

    Addind:UC1_GeometryCalculateLength File Added Successfully

---
+++ GeometrycalculatelengthUC1.java
@@ -0,0 +1,14 @@
+package LineComparison;
+
+public class GeometrycalculatelengthUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x1=4,y1=10,x2=8,y2=15;
+		double length=Math.sqrt( Math.pow((x2-x1),2)+ Math.pow((y2-y1),2) );
+
+		System.out.println("Length of the line= "+length);
+
+	}
+
+}
